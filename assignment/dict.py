7942f4af7cf10172fd404d0d5d7b7aa4effb4c4d
Nrp02 <naruepon.nice@gmail.com>
2025-05-07 17:40:52 +0700

    add dict

--- assignment/dict.py
+++ assignment/dict.py
@@ -6,3 +6,12 @@ def showBill():
     for number in range(len(menuList)):
         print(menuList[number][0], menuList[number][1])
     print("Total :", price,"Baht")
+
+while True:
+    menuName = input("Plese Enter Menu :")
+    if(menuName.lower() == "exit"):
+        break
+    else:
+        menuList.append([menuName, systemMenu[menuName]])
+        price += int(systemMenu[menuName])
+showBill()
